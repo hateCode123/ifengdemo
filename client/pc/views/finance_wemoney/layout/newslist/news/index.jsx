3ed7563f31ce311c0d531341dc2a7bdcedf03761
shaoyf <shaoyf@ifeng.com>
2018-04-20 10:15:29 +0800

    wemoney新闻信息流

--- client/pc/views/finance_wemoney/layout/newslist/news/index.jsx
+++ client/pc/views/finance_wemoney/layout/newslist/news/index.jsx
@@ -1,62 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
-import Chip from 'Chip';
-/**
- * 定义 News 组件
- */
-class News extends React.Component {
+
+class News extends React.PureComponent {
     /**
      * 渲染组件
      */
     render() {
-        const  {content}  = this.props;
-//        let typeName = [];
-//        {content.map((item,index) => (
-//            //styles.unslider-active
-//            typeName.push(item.type)
-//        ))} 
+        const { content } = this.props;
 
-        const creatList = () =>(
-            <div>
-                {content.map((item,index) => (
-                    <div key={index} className={styles.listL + " "+ styles.clearfix}>
-
-                        <a href={item.url} target="_blank" className = {styles.imgStyle}><img src={item.poster} /></a>
-                        
-                        <div className={styles.listText}>
-                            <p className={styles.text}>
-                                <a href={item.url}> {item.title} </a>
-                            </p>
-                            <p className={styles.time}>
-                                <b>{item.type}</b>
-                                <span>{item.source}</span>
-                                <span>{item.date}</span> 
-                                <span>{item.time}</span>
-                            </p>
-                        </div>
-
-                        <div className={styles.ly}>
-                            <a href="#" target="_blank" ></a>
-                        </div>
-
-                    </div>
-                    
-                ))} 
-            </div>
-            )
+        console.log(content);
 
         return (
-            <div className={styles.sp}>
-            <Chip
-                id="10007" type="static" title="wemoney新闻信息流"
-                groupName="文章" content={ content}
-            >
-                {creatList()}
-            </Chip>
+            <div className={styles.contentList}>
+                {content.map((item, index) => (
+                    <div key={index} className={styles.list}>
+                        {item.poster ? (
+                            <a
+                                href={item.url}
+                                target="_blank"
+                                rel="nofollow me noopener noreferrer"
+                                className={styles.imgBox}>
+                                <img src={item.poster} width="144" height="96" className={styles.trans} />
+                            </a>
+                        ) : (
+                            ''
+                        )}
+                        <div className={styles.list_text}>
+                            <p className={styles.text}>
+                                <a
+                                    href={item.url}
+                                    target="_blank"
+                                    rel="nofollow me noopener noreferrer"
+                                    title={item.title}>
+                                    {item.title}
+                                </a>
+                            </p>
+                            <p className={styles.time}>
+                                <span className={styles[`${item.typeStyle}`]}>{item.type}</span>
+                                <span className={styles.source}>{item.source}</span>
+                                <span className={styles.date}>{item.date}</span>
+                                <span className={styles.date}>{item.time}</span>
+                            </p>
+                        </div>
+                            <div className={styles.comment}>
+                                <a href="#" target="_blank" rel="nofollow me noopener noreferrer">
+                                    1225
+                                </a>
+                            </div>
+                    </div>
+                ))}
             </div>
-
-        )
+        );
     }
 }
 
