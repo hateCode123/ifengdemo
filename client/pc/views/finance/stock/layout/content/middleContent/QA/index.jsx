2c234fdda0c7cde47b1413e28dc8795a03c8f32a
张云凯 <zhangyk3@ifeng.com>
2018-05-07 10:41:03 +0800

    分析师答疑模块

--- client/pc/views/finance/stock/layout/content/middleContent/QA/index.jsx
+++ client/pc/views/finance/stock/layout/content/middleContent/QA/index.jsx
@@ -7,8 +7,61 @@ import { MidTitle } from '../../../../components/midTitle';
 
 class Qa extends React.PureComponent {
     state = {
+        qaers: {
+            url: [],
+            img: [],
+        },
+        alist: [],
         current: 0,
     };
+
+    componentDidMount() {
+        const { tabs } = this.props;
+        const qaers = [];
+        const alist = [];
+
+        tabs.map((item, index) => {
+            if (index < 3) {
+                jsonp('http://app.finance.ifeng.com/gszb/user_ol.php', {
+                    data: {
+                        name: item.name,
+                        type: item.type,
+                    },
+                }).then(data => {
+                    qaers.push({
+                        url: data[0].url,
+                        img: data[0].image,
+                    });
+
+                    if (index === 2) {
+                        this.setState({ qaers });
+                    }
+                });
+            } else {
+                qaers.push({
+                    url: item.url,
+                    img: item.src,
+                });
+            }
+
+            jsonp('http://app.finance.ifeng.com/gszb/a_data.php', {
+                data: {
+                    name: item.name,
+                    type: item.type ? item.type : '',
+                    cb: 'getData(a_data)',
+                },
+                jsonpCallback: 'getData',
+            }).then(data => {
+                alist.push({ data });
+
+                if (index === 3) {
+                    this.setState({ alist });
+                }
+            });
+
+            return alist;
+        });
+    }
 
     handleMouseOver = index => {
         this.setState({ current: index });
@@ -18,7 +71,7 @@ class Qa extends React.PureComponent {
      * 渲染组件
      */
     render() {
-        const { current } = this.state;
+        const { current, qaers, alist } = this.state;
         const { content, tabs } = this.props;
         const { title, url } = content;
 
@@ -37,8 +90,13 @@ class Qa extends React.PureComponent {
                         ))}
                     </ul>
                     <div>
-                        <a href={tabs[current].url} target="_blank" rel={rel}>
-                            <img className={styles.user} src={tabs[current].src} target="_blank" rel={rel} />
+                        <a href={qaers.url.length > 0 ? qaers[current].url : ''} target="_blank" rel={rel}>
+                            <img
+                                className={styles.user}
+                                src={qaers.img.length > 0 ? qaers[current].img : ''}
+                                target="_blank"
+                                rel={rel}
+                            />
                         </a>
                     </div>
                 </div>
