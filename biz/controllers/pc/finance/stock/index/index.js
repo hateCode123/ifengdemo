b2157aea05fef8600b5e6f06c9d63387e9816e50
张云凯 <zhangyk3@ifeng.com>
2018-06-07 16:42:38 +0800

    修改错误接口路径

--- biz/controllers/pc/finance/stock/index/index.js
+++ biz/controllers/pc/finance/stock/index/index.js
@@ -30,7 +30,7 @@ exports.list = {
             ['subNavigation', 'KVProxy', 'getStaticFragment', 10040, getJsonByKey('content')],
 
             // 股票显示
-            ['stockPlate', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/caterankjson', getJson()],
+            ['stockPlate', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/caterank.json', getJson()],
 
             // 动画
             ['animationPic', 'KVProxy', 'getStaticFragment', 10085, getJsonByKey('content')],
@@ -148,32 +148,32 @@ exports.list = {
             ['industry', 'KVProxy', 'getStaticFragment', 10087, getJsonByKey('content')],
 
             // 股票显示
-            ['hyin', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/zijin_hyinjson', getJson()],
+            ['hyin', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/zijin_hyin.json', getJson()],
 
             // 股票显示
-            ['hyout', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/zijin_hyoutjson', getJson()],
+            ['hyout', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/zijin_hyout.json', getJson()],
 
             // 股票显示
-            ['gnin', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/zijin_gninjson', getJson()],
+            ['gnin', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/zijin_gnin.json', getJson()],
 
             // 股票显示
-            ['gnout', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/zijin_gnoutjson', getJson()],
+            ['gnout', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/zijin_gnout.json', getJson()],
 
             // 个股资金流向标题
             ['singleStockTitle', 'KVProxy', 'getStaticFragment', 10088, getJsonByKey('content')],
 
             // 市场雷达
             ['marketRadarTabs', 'KVProxy', 'getStaticFragment', 10089, getJsonByKey('content')],
-            ['marketRadar', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/marketradarjson', getJson()],
+            ['marketRadar', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/marketradar.json', getJson()],
 
             // 大单追踪
             ['trackTabs', 'KVProxy', 'getStaticFragment', 10090, getJsonByKey('content')],
-            ['track', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/bigtrackjson', getJson()],
+            ['track', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/bigtrack.json', getJson()],
 
             // 5日增减仓
             ['fiveDaysTabs', 'KVProxy', 'getStaticFragment', 10091, getJsonByKey('content')],
-            ['fiveDaysBuy', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/fiveday_buyjson', getJson()],
-            ['fiveDaysSell', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/fiveday_selljson', getJson()],
+            ['fiveDaysBuy', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/fiveday_buy.json', getJson()],
+            ['fiveDaysSell', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/fiveday_sell.json', getJson()],
 
             // 高手学堂标题
             ['schoolTitle', 'KVProxy', 'getStaticFragment', 10069, getJsonByKey('content')],
@@ -195,32 +195,38 @@ exports.list = {
 
             // 明星分析师数据
             ['starAnalyst', 'KVProxy', 'getStaticFragment', 10093, getJsonByKey('content')],
-            ['starSrank', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/star_s_rankjson', getJson()],
-            ['starMrank', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/star_m_rankjson', getJson()],
-            ['starLrank', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/star_l_rankjson', getJson()],
+            ['starSrank', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/star_s_rank.json', getJson()],
+            ['starMrank', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/star_m_rank.json', getJson()],
+            ['starLrank', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/star_l_rank.json', getJson()],
 
             // 研报选股标题
             ['stockPickingTitle', 'KVProxy', 'getStaticFragment', 10094, getJsonByKey('content')],
 
             // 机构荐股池
             ['stockpool', 'KVProxy', 'getStaticFragment', 10095, getJsonByKey('content')],
-            ['stockpoolData', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/yb_stockpooljson', getJson()],
+            ['stockpoolData', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/yb_stockpool.json', getJson()],
 
             // 目标涨幅最大
             ['chgpctest', 'KVProxy', 'getStaticFragment', 10096, getJsonByKey('content')],
-            ['chgpctestData', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/yb_chgpctestjson', getJson()],
+            ['chgpctestData', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/yb_chgpctest.json', getJson()],
 
             // 评级调高个股
             ['levelup', 'KVProxy', 'getStaticFragment', 10097, getJsonByKey('content')],
-            ['levelupData', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/yb_levelupjson', getJson()],
+            ['levelupData', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/yb_levelup.json', getJson()],
 
             // 机构首次关注股
             ['firstfocus', 'KVProxy', 'getStaticFragment', 10098, getJsonByKey('content')],
-            ['firstfocusData', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/yb_firstfocusjson', getJson()],
+            [
+                'firstfocusData',
+                'KVProxy',
+                'getSsiFragment',
+                'finance.ifeng.com/app/json/zq/yb_firstfocus.json',
+                getJson(),
+            ],
 
             // 机构关注度最高
             ['focusest', 'KVProxy', 'getStaticFragment', 10099, getJsonByKey('content')],
-            ['focusestData', 'KVProxy', 'getSsiFragment', 'financeifengcom/app/json/zq/yb_focusestjson', getJson()],
+            ['focusestData', 'KVProxy', 'getSsiFragment', 'finance.ifeng.com/app/json/zq/yb_focusest.json', getJson()],
 
             // 底部合作链接
             ['cooperation', 'KVProxy', 'getStaticFragment', 10074, getStringByKey('content')],
