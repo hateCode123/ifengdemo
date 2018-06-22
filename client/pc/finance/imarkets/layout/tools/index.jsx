2d7a1ff5a4f11bcdd1bcf30c23a810b265e1a080
张云凯 <zhangyk3@ifeng.com>
2018-06-22 11:14:52 +0800

    修改错误的文件索引

--- client/pc/finance/imarkets/layout/tools/index.jsx
+++ client/pc/finance/imarkets/layout/tools/index.jsx
@@ -6,37 +6,42 @@ import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
 import Calendar from './calender';
-import Search from './Search';
+import Search from './search';
 
-class Tools extends PureComponent{
+class Tools extends PureComponent {
     state = {
-        isActive: null  // 行情中心是否显示
+        isActive: null, // 行情中心是否显示
     };
 
     // 显示行情中心
-    handleDisplayHangqingzhongxinClick = (event) => this.setState({ isActive: !this.state.isActive });
-    render(){
+    handleDisplayHangqingzhongxinClick = event => this.setState({ isActive: !this.state.isActive });
+    render() {
         return (
             <Fragment>
-                <div className={ styles.tools_box }>
-                    <div className={ styles.tools_main }>
+                <div className={styles.tools_box}>
+                    <div className={styles.tools_main}>
                         {/* 财经日历 */}
                         <Calendar />
                         {/* 全球直播 */}
-                        <a className={ `${ styles.ml14 } ${ styles.zhibo_link }` } href="http://finance.ifeng.com/gold/zhibo/" target="_blank">
-                            <i className={ styles.earth } />
-                            <span className={ styles.link_title }>全球直播</span>
-                            <i className={ styles.play } />
+                        <a
+                            className={`${styles.ml14} ${styles.zhibo_link}`}
+                            href="http://finance.ifeng.com/gold/zhibo/"
+                            target="_blank">
+                            <i className={styles.earth} />
+                            <span className={styles.link_title}>全球直播</span>
+                            <i className={styles.play} />
                         </a>
                         {/* 跳转搜索 */}
                         <Search />
                     </div>
                 </div>
-                {
-                    this.state.isActive !== null ? (
-                        <iframe className={ styles.hangqingzhongxin } style={{ display: this.state.isActive ? 'block' : 'none' }} src="http://fx.caiku.com/coop/ifeng/pair_new" />
-                    ) : null
-                }
+                {this.state.isActive !== null ? (
+                    <iframe
+                        className={styles.hangqingzhongxin}
+                        style={{ display: this.state.isActive ? 'block' : 'none' }}
+                        src="http://fx.caiku.com/coop/ifeng/pair_new"
+                    />
+                ) : null}
             </Fragment>
         );
     }
