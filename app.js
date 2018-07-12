295c2ac764409706423cf115f27c0a113618f0ad
liruiqing <lirq1@ifeng.com>
2018-07-12 15:13:35 +0800

    feat: 关闭jaeger，打开prom和日志

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ if (config.default.statistics) {
     // 监控请求响应时间，catch未知的错误
     app.use(async (ctx, next) => {
         // 请求进入
-        // logger.info(`<-- ${ctx.method} ${ctx.originalUrl}`);
+        logger.info(`<-- ${ctx.method} ${ctx.originalUrl}`);
         const start = new Date();
         ctx.routerTimeStart = process.hrtime();
         ctx.rpcTimeList = [[], []];
@@ -197,18 +197,18 @@ if (config.default.statistics) {
         parseTime = parseTime.toFixed(3);
         ctx.rpc_time = rpcTime;
         ctx.parse_time = parseTime;
-        // logger.info(
-        //     `--> ${ctx.method} ${ctx.originalUrl} ${ctx.status} - ${ms}ms - router: ${
-        //         ctx.routerTimeEnd
-        //     }ms - rpc:[${rpcCount}, ${rpcTime}ms] - JSON.parse: [${
-        //         ctx.parseTime.length
-        //     }, ${parseTime}ms] - domain: ${ctx.header.domain || ''}`,
-        // );
+        logger.info(
+            `--> ${ctx.method} ${ctx.originalUrl} ${ctx.status} - ${ms}ms - router: ${
+                ctx.routerTimeEnd
+            }ms - rpc:[${rpcCount}, ${rpcTime}ms] - JSON.parse: [${
+                ctx.parseTime.length
+            }, ${parseTime}ms] - domain: ${ctx.header.domain || ''}`,
+        );
     });
 } else {
     // 监控请求响应时间，catch未知的错误
     app.use(async (ctx, next) => {
-        // logger.debug(`<-- ${ctx.method} ${ctx.originalUrl}`);
+        logger.debug(`<-- ${ctx.method} ${ctx.originalUrl}`);
         const start = new Date();
 
         try {
@@ -231,7 +231,7 @@ if (config.default.statistics) {
         const ms = new Date() - start;
         ctx.requestTime = ms;
 
-        // logger.debug(`--> ${ctx.method} ${ctx.originalUrl} ${ctx.status} - ${ms}ms`);
+        logger.debug(`--> ${ctx.method} ${ctx.originalUrl} ${ctx.status} - ${ms}ms`);
     });
 }
 // app.use(prevent);
