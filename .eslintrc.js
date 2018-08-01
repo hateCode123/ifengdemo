8096108a44540edbba6653f3050b9fa00fcb3692
张云凯 <zhangyk3@ifeng.com>
2018-08-01 14:27:41 +0800

    perf: eslint 规则利用 npm 包统一管理

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:flowtype/recommended'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true,
+        },
+        sourceType: 'module',
+    },
+    plugins: ['react', 'flowtype'],
+    rules: require('@ifeng/eslint'),
+};
