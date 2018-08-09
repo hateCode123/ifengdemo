e7cf11dfe44112b9d348ab9da7bcea79151c6f0d
liruiqing <lirq1@ifeng.com>
2018-08-09 14:27:19 +0800

    feat: 生产测试环境日志输出到控制台，目前生产和测试只打印info（含）已上级别的日志，

--- biz/common/logger.js
+++ biz/common/logger.js
@@ -4,19 +4,26 @@ const logs = config.default.logs;
 
 let streams = [];
 
-if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') {
-    logs.stream = process.stdout;
-    streams.push(logs);
-} else {
-    streams = logs;
-}
+// if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging') {
+//     logs.stream = process.stdout;
+//     streams.push(logs);
+// } else {
+//     streams = logs;
+// }
+
+logs.stream = process.stdout;
+streams.push(logs);
 
 const logger = bunyan.createLogger({
     name: 'app',
     streams,
 });
 
+console.trace = logger.trace.bind(logger);
 console.log = logger.debug.bind(logger);
+console.debug = logger.debug.bind(logger);
+console.info = logger.info.bind(logger);
+console.warn = logger.warn.bind(logger);
 console.error = logger.error.bind(logger);
 
 module.exports = logger;
