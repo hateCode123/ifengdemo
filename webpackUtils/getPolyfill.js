a9309c29d8b0d23a62c097fd7eeb3ab3c4d33675
liruiqing <lirq1@ifeng.com>
2018-08-15 15:40:30 +0800

    style: 更新polyfile文件

--- webpackUtils/getPolyfill.js
+++ webpackUtils/getPolyfill.js
@@ -23,19 +23,5 @@ module.exports = level => {
 <![endif]-->`;
     }
 
-    return `
-<!--[if lt IE 9]>
-<!-- build:js ie9.polyfill.min.js -->
-        <script src="/polyfill/textContent.js"></script>
-        <script src="/polyfill/now.js"></script>
-        <script src="/polyfill/getComputedStyle.js"></script>
-    <!-- endbuild -->
-<![endif]-->
-<!--[if lt IE 9]>
-    <!-- build:js es.polyfill.min.js -->
-        <script src="/polyfill/lib/es5-shim.min.js"></script>
-        <script src="/polyfill/lib/es5-sham.min.js"></script>
-        <script src="/polyfill/lib/console-polyfill.min.js"></script>
-    <!-- endbuild -->
-<![endif]-->`;
+    return '';
 };
