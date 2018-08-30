e52e0a422f320777c399d3c73d53a5242ba46c1a
duan602728596 <duanhaochen@126.com>
2018-08-30 16:11:23 +0800

    fix: imarkets搜索按钮居中
    fix #76

--- client/pc/finance/imarkets/layout/tools/search/index.jsx
+++ client/pc/finance/imarkets/layout/tools/search/index.jsx
@@ -6,41 +6,45 @@ import React, { PureComponent, createRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
 
-class Search extends PureComponent{
+class Search extends PureComponent {
     searchTextRef = createRef();
 
     // 点击跳转新页面搜索
-    handleSearchClick = (event)=>{
+    handleSearchClick = event => {
         const value = this.searchTextRef.current.value;
-        window.open(`http://search.ifeng.com/sofeng/search.action?q=${ value }&c=1`);
+
+        window.open(`http://search.ifeng.com/sofeng/search.action?q=${value}&c=1`);
     };
     // input有焦点时，如果值是“search”，值变成空
-    handleSearchFocus = (event)=>{
+    handleSearchFocus = event => {
         const current = this.searchTextRef.current;
         const value = current.value;
-        if(value === 'search'){
+
+        if (value === 'search') {
             current.value = '';
         }
     };
     // input失去焦点时，如果没有值，值为“search”
-    handleSearchBlur = (event)=>{
+    handleSearchBlur = event => {
         const current = this.searchTextRef.current;
         const value = current.value;
-        if(value === ''){
+
+        if (value === '') {
             current.value = 'search';
         }
     };
-    render(){
+    render() {
         return (
-            <div className={ styles.search_box }>
-                <input 
-                    className={ styles.search_text }
-                    ref={ this.searchTextRef } type="text"
+            <div className={styles.search_box}>
+                <input
+                    className={styles.search_text}
+                    ref={this.searchTextRef}
+                    type="text"
                     defaultValue="search"
-                    onFocus={ this.handleSearchFocus }
-                    onBlur={ this.handleSearchBlur }
+                    onFocus={this.handleSearchFocus}
+                    onBlur={this.handleSearchBlur}
                 />
-                <input className={ styles.sousuo_btn } type="button" onClick={ this.handleSearchClick } />
+                <input className={styles.sousuo_btn} type="button" onClick={this.handleSearchClick} />
             </div>
         );
     }
