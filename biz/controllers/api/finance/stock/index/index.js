ff25073ea3e3ba0920d778bd3400b4bb3c3d8dd4
张云凯 <zhangyk3@ifeng.com>
2018-08-31 08:56:32 +0800

    feat: 增加股首api文件

---
+++ biz/controllers/api/finance/stock/index/index.js
@@ -0,0 +1,8 @@
+const { KVProxy } = require('../../../../../providers/ucmsapiProxy');
+const {
+    jsonParse,
+    handleData,
+    handleJson,
+    handleJsonByKey,
+    handleJs,
+} = require('../../../../../services/common/common');
