ae7feabe6892cb862f45ae6cfc0c6d767fd559c0
shaoyf <shaoyf@ifeng.com>
2018-09-05 14:23:35 +0800

    fix: 投资观察页面组件微调

--- client/pc/finance/stock/gstzgc/layout/index.jsx
+++ client/pc/finance/stock/gstzgc/layout/index.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../reset.css';
 import styles from './index.css';
 import Chip from 'Chip';
 import ChipEdit from 'ChipEdit';
 import transform from 'chipDataTransform';
+import Recommend from '../components/recommend';
+
+import { Header } from './header';
 import Navigation from './navigation';
 import Topic from './topic/';
 import Headline from './headline';
+import { Slide } from './slide';
 import PaperContent from './paperContent';
 import TitleR from './../components/titleR';
 import { Live } from './live';
-import ClickRank from './clickRank';
-import Qa from './QA';
-import { Header } from './header';
-import { BottomFooter } from './footer';
-import { Slide } from './slide';
 import StockHq from './stockHq';
-import Recommend from '../components/recommend';
+import Qa from './QA';
+import ClickRank from './clickRank';
+import { BottomFooter } from './footer';
 
 class Layout extends React.PureComponent {
     static propTypes = {
