c7185682a691c352e36075a25c229ffadda21277
shaoyf <shaoyf@ifeng.com>
2018-09-10 10:46:54 +0800

    fix: 组件结构调整-wemoney

--- client/pc/finance/wemoney/layout/Navigation/index.jsx
+++ client/pc/finance/wemoney/layout/Navigation/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
 import Chip from 'Chip';
-import Recommend from '../recommend/';
+import Recommend from '../../compontents/recommend';
 import Search from './search/';
 import errorBoundary from '@ifeng/errorBoundary';
 
