edd70351ca8e68bd9793145926551ccd284c4a97
zhujian <zhujian@ifeng.com>
2018-10-17 17:56:56 +0800

    fix: 修改港股首页获取数据key值

--- biz/controllers/pc/finance/hk/index/index.js
+++ biz/controllers/pc/finance/hk/index/index.js
@@ -82,47 +82,40 @@ exports.list = {
             ['hk_tt_recommend', 'KVProxy', 'getRecommendFragment', 55015, getJsonByKey('data')],
 
             // 港股头条信息流(排重推荐位)
-            ['hk_tt_withoutR', 'KVProxy', 'getDynamicFragment', 40032, getJsonByKey('data')],
+            ['hk_tt_withoutR', 'KVProxy', 'getDynamicFragment', '1-69-35248-', getJsonByKey('data')],
 
             // 打新攻略信息流(做单图处理)
-            ['hk_dzgl_singlePic', 'KVProxy', 'getDynamicFragment', 40031, getJsonByKey('data')],
+            ['hk_dzgl_singlePic', 'KVProxy', 'getDynamicFragment', '1-69-35250-', getJsonByKey('data')],
 
             // 新股点评信息流
-            // ??
-            ['hk_xgdp', 'KVProxy', 'getDynamicFragment', 35252, getJsonByKey('data')],
+            ['hk_xgdp', 'KVProxy', 'getDynamicFragment', '1-69-35252-', getJsonByKey('data')],
 
             // 新股评级信息流(做单图处理)
-            ['hk_xgpj', 'KVProxy', 'getDynamicFragment', 40033, getJsonByKey('data')],
+            ['hk_xgpj', 'KVProxy', 'getDynamicFragment', '1-69-35251-', getJsonByKey('data')],
 
             // 热点专题信息流(做单图处理)
-            ['hk_rdzt', 'KVProxy', 'getDynamicFragment', 40035, getJsonByKey('data')],
+            ['hk_rdzt', 'KVProxy', 'getDynamicFragment', '1-69-35256-', getJsonByKey('data')],
 
             // 上市公司信息流(做单图处理)
-            // ??
-            ['hk_ssgs', 'KVProxy', 'getDynamicFragment', 35255, getJsonByKey('data')],
+            ['hk_ssgs', 'KVProxy', 'getDynamicFragment', '1-69-35255-', getJsonByKey('data')],
 
             // 港股要闻信息流
-            ['hk_ggyw', 'KVProxy', 'getDynamicFragment', 40036, getJsonByKey('data')],
+            ['hk_ggyw', 'KVProxy', 'getDynamicFragment', '1-69-41-', getJsonByKey('data')],
 
             // 美股要闻信息流
-            // ??
-            ['hk_mgyw', 'KVProxy', 'getDynamicFragment', 35259, getJsonByKey('data')],
+            ['hk_mgyw', 'KVProxy', 'getDynamicFragment', '1-69-35259-', getJsonByKey('data')],
 
             // 机构动态信息流(做单图处理)
-            // ??
-            ['hk_jgdt', 'KVProxy', 'getDynamicFragment', 35260, getJsonByKey('data')],
+            ['hk_jgdt', 'KVProxy', 'getDynamicFragment', '1-69-35260-', getJsonByKey('data')],
 
             // 港股学堂信息流(做单图处理)
-            // ??
-            ['hk_ggxt', 'KVProxy', 'getDynamicFragment', 35253, getJsonByKey('data')],
+            ['hk_ggxt', 'KVProxy', 'getDynamicFragment', '1-69-35253-', getJsonByKey('data')],
 
             // 名家有话说信息流(做单图处理)
-            // ??
-            ['hk_mjyhs', 'KVProxy', 'getDynamicFragment', 35258, getJsonByKey('data')],
+            ['hk_mjyhs', 'KVProxy', 'getDynamicFragment', '1-69-35258-', getJsonByKey('data')],
 
             // 轮证资讯信息流
-            // ??
-            ['hk_lzzx', 'KVProxy', 'getDynamicFragment', 35249, getJsonByKey('data')],
+            ['hk_lzzx', 'KVProxy', 'getDynamicFragment', '1-69-35249-', getJsonByKey('data')],
         ];
 
         const allData = await transfer(ctx, json);
