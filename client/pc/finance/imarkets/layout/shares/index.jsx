efa76c7aa0ac1e7d4cf15ca30e57ec2d6af41cd4
duan602728596 <duanhaochen@126.com>
2018-10-19 10:36:31 +0800

    perf: 调整imarkets

--- client/pc/finance/imarkets/layout/shares/index.jsx
+++ client/pc/finance/imarkets/layout/shares/index.jsx
@@ -6,14 +6,21 @@ import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import styles from './index.css';
 
-class Shares extends PureComponent{
-    render(){
+class Shares extends PureComponent {
+    render() {
         return (
-            <div className={ styles.shares }>
-                <iframe className={ styles.iframe } src="http://unews.fx678.com/union/HaoSTZ/EC/Fhw/FhwInterface.html" scrolling="no" />
+            <div className={styles.shares}>
+                <iframe
+                    className={styles.iframe}
+                    src="http://unews.fx678.com/union/HaoSTZ/EC/Fhw/FhwInterface.html"
+                    scrolling="no"
+                    frameBorder="0"
+                />
                 <p>
                     数据来源：
-                    <a href="http://www.fx678.com/" target="_blank">汇通网</a>
+                    <a href="http://www.fx678.com/" target="_blank">
+                        汇通网
+                    </a>
                 </p>
             </div>
         );
